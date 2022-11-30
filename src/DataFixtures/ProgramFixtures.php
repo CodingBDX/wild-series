1cd882a6f81765cf2cd7a951fa544bc274706a24
CodingBDX <antonycharier@gmail.com>
2022-11-30 13:09:32 +0000

    wild-serie fixture advanced with relations

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Program;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ProgramFixtures extends Fixture
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $program = new Program();
+
+        $program->setTitle('david boos');
+
+        $program->setSynopsis('formateur js');
+
+        $program->setCategory($this->getReference('category_romance'));
+
+        $manager->persist($program);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
